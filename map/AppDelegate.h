22d13f7578cd53eb6b41cb2f55252b98ce42665c
mac <mac@macdeMac-mini.local>
2015-07-23 10:08:41 +0800

    Initial Commit

---
+++ map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  map
+//
+//  Created by mac on 15/7/23.
+//  Copyright (c) 2015年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  map
+//
+//  Created by mac on 15/7/23.
+//  Copyright (c) 2015年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
